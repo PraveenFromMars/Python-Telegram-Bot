1369b2625ce27c92e67673a8f39a8fe0c6c59cfa
PraveenFromMars <101979668+PraveenFromMars@users.noreply.github.com>
2022-07-09 01:56:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+from telegram.ext import *
+from telegram import *
+import os
+from os.path import *
+import requests
+import shutil
+from apiclient.discovery import build
+import string
+
+key = "AIzaSyDzeub-LYJUCp0KqZBfjQTsyB6sg76Csv0"
+
+resource = build("customsearch", 'v1', developerKey=key).cse()
+
+
+key = "gc5zTVqehcysZ6BaqnYBeHcduIoIUTNo4zXPH20SRe0"
+
+bot = Bot("5401805479:AAEIllhooQ7C0OBtoCIMS4f3NQCDArxmHsI")
+
+# print("bot has started")
+updater = Updater("5401805479:AAEIllhooQ7C0OBtoCIMS4f3NQCDArxmHsI", use_context=True)
+
+dispatcher = updater.dispatcher
+
+def reply(update:Update, context:CallbackContext):
+    bot.send_message(chat_id=update.effective_chat.id, text="hello bro how are you")
+
+def start(update:Update, context:CallbackContext):
+    bot.send_message(chat_id=update.effective_chat.id,
+        text = "Helle there, Welcome to the Image Bot. Here you can get every images with simple query. For more information fire /help"
+        )
+def Help(update:Update, context:CallbackContext):
+    bot.send_message(chat_id=update.effective_chat.id,
+        text="Hello (From search asssitant), There are 2 parameters that you can use to get the best image as possible\ncount : Number of images that you want to get 1 - 10\nsite : specific site name to get image \nIMPORTANT: All tags should be described after the search query. Example 'Dog count:3 site:pixelbay,unplash'"
+        )
+def reply(update, contet):
+    text = str(update.message.text).lower()
+    countnum = ""
+    site = []
+    sites = ""
+    query = text.split();
+    querys = ""
+    if int(text.find("count")) != -1:
+        contentintext = text.split()
+        for i in contentintext:
+            if int(i.find("count")) != -1:
+                query.remove(i)
+                contentsini = i.split(":")
+                for j in contentsini:
+                    if j != "count":
+                        countnum = int(j);
+        if countnum > 10:
+            bot.send_message(chat_id=update.effective_chat.id, text="Count limit overflowed")
+            return 
+    if int(text.find("site")) != -1:
+        contentintext = text.split()
+        for i in contentintext:
+            if int(i.find("site")) != -1:
+                query.remove(i)
+                contentsini = i.split(":")
+                for j in contentsini:
+                    if j == "site":
+                        contentsini.remove(j)
+                site = contentsini[0].split(",")
+                for k in site:
+                    if sites == "":
+                        sites = sites + k
+                    else:
+                        sites = sites + ',' + k
+    
+    for j in query:
+        querys = querys + ' ' + j
+    if int(text.find("site")) != -1:
+        querys = querys + ' ' + "inurl:" + sites;
+    result = resource.list(q=querys, cx="4d2a535d6bf66b50a", searchType='image').execute()
+    image = "hello"
+    typeOfImage = "type"
+    for item in result['items']:
+        if int(text.find("count")) != -1:
+            countnum = countnum - 1;
+        image = item['link']
+        typeOfImage = image.split()
+        bot.send_photo(chat_id=update.effective_chat.id, photo=image)
+        if int(text.find("count")) == -1:
+            break
+        else:
+            if countnum == 0:
+                break
+        
+def error(update, context):
+   logger.warning("Some internal error occured!!, please try a new key word")
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', Help))
+dispatcher.add_handler(MessageHandler(Filters.text, reply))
+# print("Bot has started!!")
+
+dispatcher.add_error_handler(error)
+
+
+updater.start_polling()
